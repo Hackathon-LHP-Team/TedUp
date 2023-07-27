f3ef8bc95c3537b634b087eeaa4ade96dfca48c6
kyle-paul <kylepaul1501@gmail.com>
2023-07-27 22:57:10 +0700

    initial ui

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ Homepage
     - Main theme:
         - Motion image 
         - "explore now" button
-    - Our Story
+    - Our Story / Introduction (With Virtual Therapist, you've got someone in your corner) -> 4 main features
     - Demo (two options: video or gif image with options bar) -> Small conversation, meaningful progress
     - Core/key features (images needed):
         - Chatbot as virtual therapist
@@ -31,3 +31,70 @@ App page:
         - System messages
         - Blogs (optional)
 '''
+
+'''
+cookies = json.loads(open("cookies.json", encoding="utf-8").read()) 
+
+
+
+async def main(res):
+    for _ in range(1):
+        bot = await Chatbot.create(cookies=cookies) 
+        input_text = input("Talk something...")
+        response = await bot.ask(prompt=input_text, conversation_style=ConversationStyle.creative, simplify_response=True)
+        bot_response = response["text"]
+        output_response = re.sub('\[\^\d+\^\]', '', bot_response)
+        res = output_response
+        await bot.close()
+    return res
+
+if __name__ == "__main__":
+    res = ""
+    res = asyncio.run(main(res))
+    print(res)
+'''
+
+
+from flask import Flask, render_template, request, jsonify
+import asyncio, json
+from EdgeGPT.EdgeGPT import Chatbot, ConversationStyle
+import re
+
+
+cookies = json.loads(open("cookies.json", encoding="utf-8").read()) 
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('home.html')
+
+@app.route("/app_experience")
+def app_experience():
+    return render_template('app_experience.html')
+
+
+async def main(res):
+    for _ in range(1):
+        bot = await Chatbot.create(cookies=cookies) 
+        input_text = input("Talk something...")
+        response = await bot.ask(prompt=input_text, conversation_style=ConversationStyle.creative, simplify_response=True)
+        bot_response = response["text"]
+        output_response = re.sub('\[\^\d+\^\]', '', bot_response)
+        res = output_response
+        await bot.close()
+    return res
+
+@app.route('/chat', methods=['GET', 'POST'])
+def chat():
+    if request.method == 'POST':
+        text = request.form.get('floatingTextarea2')
+        print(text)
+        loop = asyncio.get_event_loop()
+        res = loop.run_until_complete(main(res))
+        print(res)
+        return render_template('app_experience.html', res=res)
+    else:
+        return render_template('app_experience.html')
+
+if __name__ == '__main__':
+    app.run()
